0ed740a578a39040b5b0750c5e6cf593ed6866df
KIMSEI1124 <workju1124@gmail.com>
2024-03-16 20:54:37 +0900

    Feat: Application 추가

---
+++ src/main/java/com/cdd/sangchupassport/SangchuPassportApplication.java
@@ -0,0 +1,13 @@
+package com.cdd.sangchupassport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SangchuPassportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SangchuPassportApplication.class, args);
+    }
+
+}
