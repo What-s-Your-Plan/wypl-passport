f9282a36d74f913fa15501999355c75a9f9fdd6b
KIMSEI1124 <workju1124@gmail.com>
2024-04-10 23:46:23 +0900

    Refactor: 주석 작성
    추후 보고 이해하기 쉽도록 주석을 작성

--- src/main/java/com/cdd/sangchupassport/support/PassportValidator.java
+++ src/main/java/com/cdd/sangchupassport/support/PassportValidator.java
@@ -1,6 +1,5 @@
 package com.cdd.sangchupassport.support;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
 
@@ -8,34 +7,44 @@ import com.cdd.sangchupassport.Passport;
 import com.cdd.sangchupassport.exception.PassportErrorCode;
 import com.cdd.sangchupassport.exception.PassportException;
 import com.cdd.sangchupassport.token.PassportToken;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
 public class PassportValidator {
+	/**
+	 * Redis Repository
+	 */
 	private final CrudRepository<PassportToken, String> repository;
-	private final ObjectMapper objectMapper;
 
 	public PassportValidator(
-			CrudRepository<PassportToken, String> repository,
-			@Autowired ObjectMapper mapper
+		final CrudRepository<PassportToken, String> repository
 	) {
 		this.repository = repository;
-		objectMapper = mapper;
 	}
 
-	public void validatePassport(final Passport passport) {
+	/**
+	 * `Passport`가 만료되었는지, 재사용되었는지 확인합니다.
+	 *
+	 * @param passport 검증되는 인증 객체
+	 */
+	public void validatePassport(
+		final Passport passport
+	) {
 		validateExpiredTime(passport);
 		validateReuse(passport);
 	}
 
-	private void validateExpiredTime(final Passport passport) {
+	private void validateExpiredTime(
+		final Passport passport
+	) {
 		long now = System.currentTimeMillis();
 		if (now > passport.getExpiredTime()) {
 			throw new PassportException(passport, PassportErrorCode.EXPIRED_PASSPORT);
 		}
 	}
 
-	private void validateReuse(final Passport passport) {
+	private void validateReuse(
+		final Passport passport
+	) {
 		if (repository.existsById(passport.getId())) {
 			throw new PassportException(passport, PassportErrorCode.REUSE_PASSPORT);
 		}
